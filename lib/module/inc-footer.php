8e10aae97a559fc5fc672bb6afb3f89d10a5c80e
akitacoinorg <141422294+akitacoinorg@users.noreply.github.com>
2023-08-04 17:30:32 +0300

    Add files via upload

---
+++ lib/module/inc-footer.php
@@ -0,0 +1,99 @@
+<footer>
+<div class="footer-con-container">
+	<div class="footer-con-overlay darkmode-header">
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class="footer-con-header">
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class="footer-ico-logo darkmode-footer-logo">
+					</div>
+					<table class="footer-table-out">
+					<tr>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Main</span>
+									</div>
+									
+									<div class="footer-tx2-bound">
+										<a href="/about">
+										<span>About</span>
+										</a>
+									</div>
+									
+									
+									
+									<div class="footer-tx2-bound">
+										<a href="/roadmap">
+										<span>Roadmap</span>
+										</a>
+									</div>
+									
+									
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Social</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="http://twitter.com/akitacoinworld" target="_blank">
+										<span>Twitter</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://t.me/akitacoinworld" target="_blank">
+										<span>Telegram</span>
+										</a>
+									</div>
+									
+									
+									
+								</div>
+							</div>
+						</td>
+						
+					</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class='footer-con-credits'>
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class='footer-con-seprator'>
+					</div>
+					<div class="footer-con-foot">
+						
+						<div class="footer-con-side">
+							<div class="footer-tx1-webmaster">
+								<div class="footer-ico-developer" style="background: url('/img/users/akita-22 (1).png') center / cover no-repeat;">
+								</div>
+								<a href="https://daginatsuko.com" target="_blank">
+								<span>Website by Akitacoin © 2023</span>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
